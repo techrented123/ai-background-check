c9b2898f7a6cf393da4f45b68f76746a7a55b712
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-11 11:43:32 -0700

    feat: add header

---
+++ app/_components/Header.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="shadow-sm text-white">
+      <div className="container mx-auto px-4 py-2">
+        <div className="">
+          <div className="flex items-center justify-between md:justify-start md:space-x-2">
+            <Image
+              src="https://rented123-brand-files.s3.us-west-2.amazonaws.com/logo_white.svg"
+              alt="logo"
+              width="80"
+              height={80}
+              className="w-15 h-15 md:w-20 md:h-20"
+            />
+
+            <div>
+              <h1 className="text-lg md:text-2xl font-bold text-[#293074]">
+                Rented123
+              </h1>
+              <p className="text-sm text-[#293074] hidden md:block">
+                AI Powered Background Verification
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
