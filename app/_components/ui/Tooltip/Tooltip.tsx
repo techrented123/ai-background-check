2abed551e9d838b7535810b9d784c71f3f89c37f
Tambi <tambi.asawo@verticalmotion.ca>
2025-06-13 15:35:24 -0700

    fix(css)

--- app/_components/ui/Tooltip/Tooltip.tsx
+++ app/_components/ui/Tooltip/Tooltip.tsx
@@ -4,21 +4,23 @@ import styles from "./Tooltip.module.css";
 const Tooltip = ({
   text,
   children,
+  position = "!right-0 !top-[-35px]",
 }: {
   text: string | React.ReactNode;
   children: React.ReactNode;
+  position?: string;
 }) => {
   const [visible, setVisible] = useState(false);
 
   return (
     <div
-      className={styles.tooltipContainer}
+      className={` ${styles.tooltipContainer}`}
       onClick={() => setVisible((prev) => !prev)}
       onMouseEnter={() => setVisible(true)}
       onMouseLeave={() => setVisible(false)}
     >
       {children}
-      {visible && <div className={styles.tooltip}>{text}</div>}
+      {visible && <div className={` ${position} ${styles.tooltip}`}>{text}</div>}
     </div>
   );
 };
