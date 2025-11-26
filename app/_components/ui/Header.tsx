2fe3dabce0dc646c1c384653329dbf5f6407c1fa
tambiasawo <tambi.asawo@yahoo.com>
2025-11-26 13:25:22 -0800

    fix:css

--- app/_components/ui/Header.tsx
+++ app/_components/ui/Header.tsx
@@ -6,22 +6,25 @@ const Header: React.FC = () => {
     <header className="shadow-sm text-white">
       <div className="container mx-auto px-4 py-2">
         <div className="">
-          <div className="flex items-center justify-between md:justify-start md:space-x-2">
-            <Image
-              src="https://rented123-brand-files.s3.us-west-2.amazonaws.com/logo_white.svg"
-              alt="logo"
-              width="80"
-              height={80}
-              className="w-15 h-15 md:w-20 md:h-20"
-            />
-
-            <div>
+          <div className="flex flex-row-reverse md:flex-row items-center justify-between md:justify-center  md:space-x-2">
+            <div
+              className="md:absolute left-8"
+            >
               <h1 className="text-lg md:text-2xl font-bold text-[#293074]">
                 Rented123
               </h1>
-              <p className="text-sm text-[#293074] hidden md:block">
+              <p className="text-sm text-[#293074] md:block">
                 Intelligent Background Check
               </p>
+            </div>
+            <div className="flex items-center justify-center">
+              <Image
+                src="https://rented123-brand-files.s3.us-west-2.amazonaws.com/logo_white.svg"
+                alt="logo"
+                width="80"
+                height={80}
+                className="w-15 h-15 md:w-20 md:h-20 text-center"
+              />
             </div>
           </div>
         </div>
